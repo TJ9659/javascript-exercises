59545b00ca6a38afe3fac17600b78432090a7e71
TJ9659 <173524552+TJ9659@users.noreply.github.com>
2025-12-02 13:02:41 +0800

    Finished 10, 11, and 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (fibNumStr) {
+
+    const fibNum = Number(fibNumStr);
+
+    if(fibNum < 0){
+        return "OOPS";
+    }
+
+    if (fibNum === 0) {
+        return 0;
+    }
+    
+    if (fibNum === 1 || fibNum === 2) {
+        return 1;
+    }
+
+
+    return fibonacci(fibNum - 2) + fibonacci(fibNum - 1);
 
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,7 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.sort((personA, personB) => 
+        ((personA.yearOfDeath || new Date().getFullYear()) - personA.yearOfBirth) > ((personB.yearOfDeath || new Date().getFullYear()) - personB.yearOfBirth) ? 1 : -1
+    )[people.length-1];
 
 };
 
